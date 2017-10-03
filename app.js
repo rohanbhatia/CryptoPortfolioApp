9ca6751ab490d703117247af00296a50f085e5fa
Rohan Bhatia <rohan1413@gmail.com>
2017-10-02 22:28:21 -0400

    mysql query work 4

--- app.js
+++ app.js
@@ -15,29 +15,21 @@ app.get('/transactions', function (req, res) {
   		host: "localhost",
   		user: "root",
   		password: "helloworld"
+  		database: "crypto_transactions"
 	});
+
 	con.connect(function(err) {
   		
   		if (err) throw err;
   		
-  		//use db
-  		con.query("USE crypto_transactions", function (err, result) {
-
-  			if (err) throw err;
-  		});
+  	
 
   		//transactions
   		con.query("SELECT * FROM transactions", function (err, result) {
     		
     		if (err) throw err;
 
-    		for (r in result) {
-
-    			for (field in r) {
-
-    				console.log(field);
-    			}
-  			}
+    		console.log(result);
   		});
   
 	});

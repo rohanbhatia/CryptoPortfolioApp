daa3af1e845ec44c216748cddbf7f3ba00f1992b
Rohan Bhatia <rohan1413@gmail.com>
2017-10-02 22:13:35 -0400

    setup db connection

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
+app.get('/transactions', function (req, res) {
+
+	//connect to db. NOTE: I'm aware this is not secure at all
+	var con = mysql.createConnection({
+  		host: "localhost",
+  		user: "root",
+  		password: "helloworld"
+	});
+
+	con.connect(function(err) {
+  	if (err) throw err;
+  		console.log("Connected!");
+	});
+	
+
+
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });

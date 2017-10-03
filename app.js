0babb74dd095ff543cf4895ff83cb5823d7c43f2
Rohan Bhatia <rohan1413@gmail.com>
2017-10-02 22:16:24 -0400

    trying query

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
+//home url
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
+//transactions
 app.get('/transactions', function (req, res) {
 
 	//connect to db. NOTE: I'm aware this is not secure at all
@@ -16,10 +18,17 @@ app.get('/transactions', function (req, res) {
 	});
 
 	con.connect(function(err) {
-  	if (err) throw err;
-  		console.log("Connected!");
+  		
+  		if (err) throw err;
+  		
+
+  		con.query("SELECT * FROM TRANSACTIONS", function (err, result) {
+    		
+    		if (err) throw err;
+    		console.log("Result: " + result);
+  		});
 	});
-	
+
 
 
 });

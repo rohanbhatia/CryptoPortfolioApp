c4d0f9840ec6c6b8b184353b19040dc6c2a156f2
Rohan Bhatia <rohan1413@gmail.com>
2017-10-04 22:36:54 -0400

    added in prices

--- scripts.js
+++ scripts.js
@@ -1,17 +1,78 @@
 
-function test() {
+function get_price(type, currency) {
 	
+	//hardcoded values for the kraken api
+	var pairname;
+	var json_pairname;
+
+	if (type == 'BTC') {
+		pairname = 'XBT' + currency;
+		json_pairname = 'XXBTZ' + currency;
+	}
+
+	if (type == 'ETH') {
+		pairname = 'ETH' + currency;
+		json_pairname = 'XETHZ' + currency;
+	}
+
+	if (type == 'LTC') {
+		pairname = 'LTC' + currency;
+		json_pairname = 'XLTCZ' + currency;
+	}
+
+	//XML Request
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://api.kraken.com/0/public/Ticker?pair=XBTUSD');
+	xhr.open('GET', ('https://api.kraken.com/0/public/Ticker?pair=' + pairname));
 	xhr.onload = function() {
     	
     	if (xhr.status === 200) {
+
+    		json = JSON.parse(xhr.responseText);
+    		price = (parseFloat(json['result'][json_pairname]['a'][0]) + parseFloat(json['result'][json_pairname]['b'][0]))/2;
         	
-        	alert(xhr.responseText);
+
+
+
+    		
+        	var children = document.getElementById(type.toLowerCase()).childNodes;
+
+        	for (child in (document.getElementById(type.toLowerCase())).childNodes) {
+
+        		//alert(child.className);
+
+        		// if (child.className == currency.toLowerCase()) {
+        		// 	alert(price);
+        		// 	//child.innerHTML = type + " Price (" + currency + "): " + toString(price);
+        		// 	break;
+        		// }
+        	}
+
+        	var next = document.getElementById(type.toLowerCase()).firstChild;
+        	while (true) {
+
+        		if (next.className == currency.toLowerCase()) {
+        
+                	next.innerHTML = type + " Price (" + currency + "): " + price.toString();
+        			break;
+        		}
+        		else {
+        			next = next.nextSibling;
+        		}
+
+
+        	}
+
+
+
     	}	
 	};
-	
+
 	xhr.send()
 };
 
-test();
+get_price("BTC", "USD");
+get_price("BTC", "CAD");
+get_price("ETH", "USD");
+get_price("ETH", "CAD");
+get_price("LTC", "USD");
+//get_price("LTC", "CAD");
